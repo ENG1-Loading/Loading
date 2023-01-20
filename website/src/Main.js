577d97511c764a265d01e2a04acfe6a719765302
scxr <cswilson@zoho.com>
2023-01-20 12:05:08 +0000

    added risk assessment page to website

--- website/src/Main.js
+++ website/src/Main.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes ,Route } from 'react-router-dom';
 import App from "./pages/WeeklyNotes";
 import Homepage from "./pages/Homepage";
+import RiskAssesment from "./pages/RiskManagement";
 const Main = () => {
     return (
         <Routes>
             <Route exact path="/" element={<App />}></Route>
             <Route exact path="/home" element={<Homepage />}></Route>
+            <Route exact path="/risk_assessment" element={<RiskAssesment />}> </Route>
         </Routes>
     )
 }
