d70af0f3584bba156dd0483b3e777b49efde85e9
scxr <cswilson@zoho.com>
2023-01-24 11:35:42 +0000

    Made homepage link to notes/executable/risk management table/version control, and created version control page

--- website/src/Main.js
+++ website/src/Main.js
@@ -3,13 +3,15 @@ import { Routes ,Route } from 'react-router-dom';
 import App from "./pages/WeeklyNotes";
 import Homepage from "./pages/Homepage";
 import RiskAssesment from "./pages/RiskManagement";
+import VersionControl from "./pages/VersionControl";
 const Main = () => {
     return (
         <Routes>
-            <Route exact path="/" element={<App />}></Route>
-            <Route exact path="/home" element={<Homepage />}></Route>
+            <Route exact path="/notes" element={<App />}></Route>
+            <Route exact path="/" element={<Homepage />}></Route>
             <Route exact path="/risk_assessment" element={<RiskAssesment />}> </Route>
-        </Routes>
+            <Route exact path="/version_control" element={<VersionControl />}></Route>
+        </Routes>   
     )
 }
 export default Main;
