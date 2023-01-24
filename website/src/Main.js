157d76ac6436d538f554eb8d40597dbc9e01cbfe
haribhandari <2012bhandari.ha@gmail.com>
2023-01-24 14:39:05 +0000

    weekly notes

--- website/src/Main.js
+++ website/src/Main.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Routes ,Route } from 'react-router-dom';
-import App from "./pages/WeeklyNotes";
+import WeeklyNotes from "./pages/WeeklyNotes";
 import Homepage from "./pages/Homepage";
 import RiskAssesment from "./pages/RiskManagement";
+import NotFound from "./pages/NotFound";
 const Main = () => {
     return (
         <Routes>
-            <Route exact path="/" element={<App />}></Route>
-            <Route exact path="/home" element={<Homepage />}></Route>
+            <Route exact path="/" element={<Homepage />}></Route>
+            <Route exact path="/weekly_notes" element={<WeeklyNotes />}></Route>
             <Route exact path="/risk_assessment" element={<RiskAssesment />}> </Route>
+        {/*    not found 404*/}
+            <Route path={"*"} element={<NotFound/>}></Route>
         </Routes>
     )
 }
---
+++ website/src/content/Notes.js
@@ -0,0 +1,81 @@
+import Week2 from '../gantt/week2.png'
+import Week3 from '../gantt/week3.png'
+import Week4 from '../gantt/week4.png'
+import Week5 from '../gantt/week5.png'
+import Week6 from '../gantt/week6.png'
+import Week7 from '../gantt/week7.png'
+import Week8 from '../gantt/week8.png'
+import Week9 from '../gantt/week9.png'
+import Week10 from '../gantt/week10.png'
+import Week11 from '../gantt/week11.png'
+import Week12 from '../gantt/week12.png'
+import Week13 from '../gantt/week13.png'
+export const Notes= () => {
+    return [
+        {
+            title:'Week2',
+            content: 'This is the content of the first note',
+            date: '07/11/2022- 13/11/2022',
+            image: Week2
+        },{
+            title:'Week3',
+            content: 'This is the content of the second note',
+            date: '14/11/2022- 20/11/2022',
+            image: Week3
+        },{
+            title:'Week4',
+            content: 'This is the content of the third note',
+            date: '21/11/2022- 27/11/2022',
+            image: Week4
+        },{
+            title:'Week5',
+            content: 'This is the content of the fourth note',
+            date: '28/11/2022- 04/12/2022',
+            image: Week5
+        },{
+            title:'Week6',
+            content: 'This is the content of the fifth note',
+            date: '05/12/2022- 11/12/2022',
+            image: Week6
+        },{
+            title:'Week7',
+            content: 'This is the content of the sixth note',
+            date: '12/12/2022- 18/12/2022',
+            image: Week7
+        },{
+            title:'Week8',
+            content: 'This is the content of the seventh note',
+            date: '19/12/2022- 25/12/2022',
+            image: Week8
+        },
+        {
+            title:'Week9',
+            content: 'This is the content of the eighth note',
+            date: '26/12/2022- 01/01/2023',
+            image: Week9
+        },
+        {
+            title:'Week10',
+            content: 'This is the content of the ninth note',
+            date: '02/01/2023- 08/01/2023',
+            image: Week10
+        },
+        {
+            title:'Week11',
+            content: 'This is the content of the tenth note',
+            date: '09/01/2023- 15/01/2023',
+            image: Week11
+        },{
+            title:'Week12',
+            content: 'This is the content of the eleventh note',
+            date: '16/01/2023- 22/01/2023',
+            image: Week12
+
+        },{
+            title:'Week13',
+            content: 'This is the content of the twelfth note',
+            date: '23/01/2023- 29/01/2023',
+            image: Week13
+        }
+    ]
+}
