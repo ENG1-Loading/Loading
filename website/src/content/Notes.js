6de9d750173a9be3cc9987e0962fa7a950c73841
Joel <104278288+JoelW2003@users.noreply.github.com>
2023-01-31 10:17:44 +0000

    Update Notes.js
    Updated dates

--- website/src/content/Notes.js
+++ website/src/content/Notes.js
@@ -15,66 +15,65 @@ export const Notes= () => {
         {
             title:'Week2',
             content: 'Planning interview with customer.',
-            date: '07/11/2022- 13/11/2022',
+            date: '16/11/2022- 22/11/2022',
             image: Week2
         },{
             title:'Week3',
             content: 'Start eliciting requirements from interview',
-            date: '14/11/2022- 20/11/2022',
+            date: '23/11/2022- 29/11/2022',
             image: Week3
         },{
             title:'Week4',
             content: 'Continue eliciting requirements and architecture',
-            date: '21/11/2022- 27/11/2022',
+            date: '30/11/2022- 06/12/2022',
             image: Week4
         },{
             title:'Week5',
             content: 'Extended architecture deadline',
-            date: '28/11/2022- 04/12/2022',
+            date: '07/11/2022- 13/12/2022',
             image: Week5
         },{
             title:'Week6',
             content: 'Started risk assessment and method selection',
-            date: '05/12/2022- 11/12/2022',
+            date: '14/12/2022- 20/12/2022',
             image: Week6
         },{
             title:'Week7',
             content: 'Section 1/2 complete',
-            date: '12/12/2022- 18/12/2022',
+            date: '21/12/2022- 26/12/2022',
             image: Week7
         },{
             title:'Week8',
             content: 'No further progress',
-            date: '19/12/2022- 25/12/2022',
+            date: '27/12/2022- 03/01/2023',
             image: Week8
         },
         {
             title:'Week9',
             content: 'Completed storyboard for game',
-            date: '26/12/2022- 01/01/2023',
+            date: '04/01/2023- 10/01/2023',
             image: Week9
         },
         {
             title:'Week10',
             content: 'Begun designing implementation',
-            date: '02/01/2023- 08/01/2023',
+            date: '11/01/2023- 17/01/2023',
             image: Week10
         },
         {
             title:'Week11',
             content: 'Started 2nd stage of architecture',
-            date: '09/01/2023- 15/01/2023',
+            date: '18/01/2023- 24/01/2023',
             image: Week11
         },{
             title:'Week12',
             content: 'Focus on finishing architecture and implementation',
-            date: '16/01/2023- 22/01/2023',
+            date: '25/01/2023- 01/02/2023',
             image: Week12
 
         },{
-            title:'Week13',
-            content: 'Final Gantt chart',
-            date: '23/01/2023- 29/01/2023',
+            title:'Final Gantt chart',
+            date: '01/02/2023',
             image: Week13
         }
     ]
