0d8e51fc65180da7e9a08ac7e02c3ec5269e4cac
Joel <104278288+JoelW2003@users.noreply.github.com>
2023-01-31 10:10:34 +0000

    Update Notes.js
    Wrote the notes for each gantt chart

--- website/src/content/Notes.js
+++ website/src/content/Notes.js
@@ -14,66 +14,66 @@ export const Notes= () => {
     return [
         {
             title:'Week2',
-            content: 'This is the content of the first note',
+            content: 'Planning interview with customer.',
             date: '07/11/2022- 13/11/2022',
             image: Week2
         },{
             title:'Week3',
-            content: 'This is the content of the second note',
+            content: 'Start eliciting requirements from interview',
             date: '14/11/2022- 20/11/2022',
             image: Week3
         },{
             title:'Week4',
-            content: 'This is the content of the third note',
+            content: 'Continue eliciting requirements and architecture',
             date: '21/11/2022- 27/11/2022',
             image: Week4
         },{
             title:'Week5',
-            content: 'This is the content of the fourth note',
+            content: 'Extended architecture deadline',
             date: '28/11/2022- 04/12/2022',
             image: Week5
         },{
             title:'Week6',
-            content: 'This is the content of the fifth note',
+            content: 'Started risk assessment and method selection',
             date: '05/12/2022- 11/12/2022',
             image: Week6
         },{
             title:'Week7',
-            content: 'This is the content of the sixth note',
+            content: 'Section 1/2 complete',
             date: '12/12/2022- 18/12/2022',
             image: Week7
         },{
             title:'Week8',
-            content: 'This is the content of the seventh note',
+            content: 'No further progress',
             date: '19/12/2022- 25/12/2022',
             image: Week8
         },
         {
             title:'Week9',
-            content: 'This is the content of the eighth note',
+            content: 'Completed storyboard for game',
             date: '26/12/2022- 01/01/2023',
             image: Week9
         },
         {
             title:'Week10',
-            content: 'This is the content of the ninth note',
+            content: 'Begun designing implementation',
             date: '02/01/2023- 08/01/2023',
             image: Week10
         },
         {
             title:'Week11',
-            content: 'This is the content of the tenth note',
+            content: 'Started 2nd stage of architecture',
             date: '09/01/2023- 15/01/2023',
             image: Week11
         },{
             title:'Week12',
-            content: 'This is the content of the eleventh note',
+            content: 'Focus on finishing architecture and implementation',
             date: '16/01/2023- 22/01/2023',
             image: Week12
 
         },{
             title:'Week13',
-            content: 'This is the content of the twelfth note',
+            content: 'Final Gantt chart',
             date: '23/01/2023- 29/01/2023',
             image: Week13
         }
