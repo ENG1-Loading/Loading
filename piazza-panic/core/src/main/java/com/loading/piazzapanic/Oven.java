b7e139ff3181cefdcd8fd5ace16b585d4a030625
Jake Keast <JRKeast2003@outlook.com>
2023-01-31 14:46:07 +0000

    ovens

---
+++ piazza-panic/core/src/main/java/com/loading/piazzapanic/Oven.java
@@ -0,0 +1,24 @@
+package com.loading.piazzapanic;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class Oven extends Entity {
+
+    public Oven(float width, float height, Body body, float posx, float posy) {
+        super(width, height, body, posx, posy);
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
